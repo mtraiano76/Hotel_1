7473cb9a76603f1e10d4acfa7fc4a8b1d17611c9
mtraiano76 <FiveStars.DevTeam@gmail.com>
2024-03-05 14:20:49 -0300

    Cambios

--- routes/nosotros.js
+++ routes/nosotros.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('nosotros');
+  res.render('nosotros',{ title: 'Nosotros' });
 });
 
 module.exports = router;
